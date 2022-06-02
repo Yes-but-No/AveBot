ffa3f62028bed0587cfff9b6a1ccaff9a8d1b2cb
Jus-Codin <justajusgamin@gmail.com>
2022-06-02 19:36:57 +0800

    Update diagnostics.py

--- AveBot/cogs/diagnostics.py
+++ AveBot/cogs/diagnostics.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from ..utils import embed_template
-
+from datetime import datetime, timezone
 from typing import TYPE_CHECKING
 
-from discord.ext.commands import Cog, command
+from discord.ext.commands import Cog, command, Context
+
+from ..utils import embed_template
 
 if TYPE_CHECKING:
   from ..bot import AveBot
@@ -15,7 +16,9 @@ class Diagnostics(Cog):
     self.bot = bot
     self.hidden = False
 
-  @command(help='Get websocket latency', guild_ids=[914057960827781130])
-  async def ping(self, ctx):
-    embed = embed_template(self.bot, ctx.author, title='Pong! \U0001F3D3', description=f'{round(self.bot.latency*1000,1)}ms')
+  @command(help='Get websocket latency')
+  async def ping(self, ctx: Context):
+    time = datetime.now(tz=timezone.utc) - ctx.message.created_at
+    time = (time.microseconds/1000 + self.bot.latency*1000)
+    embed = embed_template(self.bot, ctx.author, title='Pong! \U0001F3D3', description=f'{round(time,1)}ms')
     await ctx.reply(embed=embed)
